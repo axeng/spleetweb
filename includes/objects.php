460bb9bd08d74de661f0f57875fa030edcc2ca21
Axen Georget <mail@axen.io>
2017-02-16 21:59:13 +0100

    first base for news

--- includes/objects.php
+++ includes/objects.php
@@ -1,18 +1,77 @@
 <?php
 
-class News
-{
-	private $_name;
-	private $_date;
-	private $_author;
-
-	public function __construct($name, $date, $author)
+	class News
 	{
-		$this->_name = $name;
-		$this->_date = $date;
-		$this->_author = $author;
-	}
+		private $_name;
+		private $_date;
+		private $_author;
+		private $_text;
 
-	
-}
+		public function __construct()
+		{
+			$this->_name = "";
+			$this->_date = "";
+			$this->_author = "";
+			$this->_text = "";
+		}
+
+		public function hydrate($donnees)
+		{
+		    foreach ($donnees as $attribut => $valeur)
+		    {
+		      $methode = 'set'.ucfirst($attribut);
+		      
+		      if (is_callable([$this, $methode]))
+		      {
+		        $this->$methode($valeur);
+		      }
+		    }
+		}
+
+		public function setName($name)
+		{
+			$this->_name = $name;
+		}
+		public function getName()
+		{
+			return $this->_name;
+		}
+
+		public function setDate($date)
+		{
+			$this->_date = $date;
+		}
+		public function getDate()
+		{
+			return $this->_date;
+		}
+
+		public function setAuthor($author)
+		{
+			$this->_author = $author;
+		}
+		public function getAuthor()
+		{
+			return $this->_author;
+		}
+
+		public function setText($text)
+		{
+			$this->_text = $text;
+		}
+		public function getText()
+		{
+			return $this->_text;
+		}
+
+		public function printNews()
+		{
+			echo "name : ".$this->_name."<br />";
+			echo "date : ".$this->_date."<br />";
+			echo "author : ".$this->_author."<br />";
+			echo "text : ".$this->_text."<br />";
+		}
+
+		
+	}
 ?>
--- news.php
+++ news.php
@@ -5,18 +5,79 @@
 	<body>
 		<?php
 
-		if ($handle = opendir('news')) {
+			// Afficher les erreurs à l'écran
+			ini_set('display_errors', 1);
 
-		    while (false !== ($entry = readdir($handle))) {
+			require "includes/objects.php";
 
-		        if ($entry != "." && $entry != "..") {
+			//$news = array();
 
-		            print $entry . "<br />";
-		        }
-		    }
+			try {
 
-		    closedir($handle);
-		}
+				if ($handle = opendir('news')) {
+
+					//loop for all the files in the folder news
+				    while (false !== ($fileName = readdir($handle))) {
+
+				    	//check if the extension of the file is .news with a regex
+				    	if (preg_match("#\.news$#", $fileName)){
+				    		//open file
+				    		$file = fopen("news/".$fileName, "r");
+				    		$new = new News();
+
+				    		$balise = "";
+
+				    		//loop in all the line of the file
+				    		while (!feof($file)){
+
+				    			$line = fgets($file);
+
+				    			//if its a enter balise <...>
+				    			if (preg_match("#^<[^/].*>$#", $line))
+				    			{
+				    				$balise = str_replace("<", "", $line);
+				    				$balise = str_replace(">", "", $balise);
+				    				$balise = str_replace("\n", "", $balise);
+				    			}
+				    			//if its a exit balise </...>
+				    			elseif (preg_match("#^<[/].*>$#", $line)) 
+				    			{
+				    				$balise = "";
+				    			}
+				    			else 
+				    			{
+				    				switch ($balise) {
+				    					case "HEADER":
+				    						$args = explode(" = ", $line, 2);
+			    							$new->hydrate([
+			    									$args[0] => $args[1]
+			    								]);
+				    						break;
+
+				    					case "TEXT":
+				    						$text = $new->getText().$line;
+			    							$new->setText($text);
+				    						break;
+				    				}
+
+				    			}
+				    		}
+
+				    		//close file
+				    		fclose($file);
+				    		$news[] = $new;
+				    	}
+				    }
+				    closedir($handle);
+				}
+
+			} catch (Exception $e) {
+				print 'Exception : '.  $e->getMessage(). "<br />";
+			}
+
+			foreach ($news as $key) {
+				$key->printNews();
+			}
 
 		?>
 	</body>
