ea0b4181073783570b5239a94259c9e421787371
Axen Georget <mail@axen.io>
2017-02-27 17:41:42 +0100

    fix image

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
 
 <div align="center" style="height: 250px" >
 	<a href="index.php">
-		<img src="../images/logo_latest.png" alt="logo" style="height:100%">
+		<img src="images/logo_latest.png" alt="logo" style="height:100%">
 	</a>
 </div>
 <?php include "menu.php"; ?>
