6bd5bc308a1ae9bb259dbbe0b452f0ba3103b586
Axen Georget <axen.georget@epita.fr>
2017-04-23 17:34:54 +0200

    Update website for S2

--- project.php
+++ project.php
@@ -8,6 +8,60 @@
 	</head>
 	<body>
 		<?php include "includes/header.php"; ?>
-		PROJECT
+		<table align="center" class="newsPost">
+			<tr><td>
+				<br />
+				<h1>Bienvenue dans SPLEET !</h1>
+				<br />
+				Spleet est un jeu qui partage réflexion et action dans un univers futuriste et aseptisé. Vous contrôlez un patient qui a subi un grave traumatisme cérébrale suite un accident de voiture. Il se retrouve bloqué dans son propre subconscient. Un certain docteur dénommé Winston a une solution et vous guidera au sein de votre espace cérébrale via à une de ses inventions. Son invention est un moyen de modéliser le cerveau entier d’un humain sous la forme d’un univers. 
+				<br />
+				Le but du jeu sera de reconnecter les liaisons cérébrales du patient en résolvent des objectifs au cours des niveaux en passant d’un univers à l’autre. 
+				<br />
+				<br />
+				<br />
+				<h2>Un univers divisé en deux !</h2>
+				<br />
+				Lors de l’accident du héros, les deux hémisphères du cerveau de celui-ci se trouvent séparés. La partie gauche du cerveau, liée à la réflexion est donc complètement coupée de la partie droite, celle de la capacité de se mouvoir, l’agilité.
+				<br />
+				Dans les niveaux « de gauche » qui symbolisent la réflexion, le joueur va devoir chercher des boutons disséminés dans celui-ci cachés par différents procédés, dont notamment des énigmes. Vous serez équipé d’un pistolet laser qui permettra d’activer les boutons.
+				<br />
+				Dans les niveaux « de droite » qui symbolisent l’action, le joueur va cette fois tirer parti de ses réflexes et de son talent dans les jeux de plates-formes : en effet, ces niveaux seront constitués d’une suite de plates-formes avec un rythme de défilement élevé et truffés de pièges de différents types.
+				<br />
+				SPLEET est un véritable défi! Il se démarque des autres jeux grâce à ses deux types de jouabilité qui se rencontrent dans un seul et même monde.
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+				<h2>Un jeu avec une vraie identité graphique!</h2>
+				<br />
+				Les deux espaces « gauche » et « droite » ont une identité graphique différente. Spleet se distingue de la grande majorité des jeux vidéo par sa patte graphique très personnelle où chaque élément garde le même esprit graphique mais en même temps possède deux styles graphiques différents entre le côté action et le côté réflexion.
+				<br />
+				<br />
+				<br />
+				<h2>Narration:</h2>
+				<br />
+				Vous contrôlez un patient qui a subi un grave traumatisme cérébrale suite un accident de voiture. Il se retrouve bloqué dans son propre subconscient. Un certain docteur dénommé Winston a une solution et vous guidera au sein de votre espace cérébrale via à une de ses inventions. Il nous informe qu’avec un de ses collègue et ami nommé le docteur Churchill, ils ont travaillé sur un projet de grande envergure: un moyen de modéliser le cerveau entier d’un humain sous la forme d’un univers où le patient pourra se déplacer dans son subconscient. 
+				<br />
+				Le but du jeu sera de reconnecter les liaisons cérébrales du patient en résolvent des objectifs au cours des niveaux en passant d’un univers à l’autre. 
+				<br />
+				Winston est notre guide, tout au long de l’aventure il nous informe de la situation. De plus, il nous conseille quand on se trouve devant une impasse.
+				<br />
+				Pour que le docteur communique et nous guide tout au long du jeu, Hugo (vanilor) a eu l’idée d’inclure d’un programme afin de pouvoir y mettre des sous titres et des fichiers audio en même temps.
+				<br />
+				Nous avons donc pu placer les sous-titres aux endroits clés des niveaux.
+				<br />
+				Ces sous-titres sont fondamentales pour donner une profondeur scénaristique mais aussi pour prévenir la frustration du joueur si il se sentait perdu et ne savaient pas quoi faire dans le niveau.
+				<br />
+				<br />
+				<br />
+				<br />
+				<h2>Graphisme:</h2>
+				<br />
+				Dans un premier temps Hugo a réaliser des pièces cubiques illustrer sur ces images ci-dessous. Afin de pouvoir construire chaque niveau plus facilement comme un puzzle. 
+				<br />
+				Cette technique permet d’être plus productif que si il fallait mettre un sol puis mettre un mur à droite puis un mur à gauche et ainsi de suite… 
+			</td></tr>
+		</table>
 	</body>
 </html>
