ece0fff686b5b3a255e52503b6d91f2ff7f5a18d
Axen Georget <axen.georget@epita.fr>
2017-06-06 16:04:04 +0200

    Add some pages and documents

---
+++ members.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta name="viewport" content="width=device-width" />
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<?php include"styles/style.php"; ?>
+		<title>Membres</title>
+	</head>
+	<body>
+		<?php include "includes/header.php"; ?>
+		<table align="center" class="contentBody">
+				<tr><td>
+					<h1>Hugo "Vanillor" TRITSCH</h1>
+				    J’ai fait une Terminale SVT spécialité Physique. Je n’ai donc pas eu de formation informatique auparavant. Cependant, j’ai toujours aimé apprendre l’informatique par moi-même, j’ai notamment appris le HTML/CSS. De plus, depuis mon plus jeune âge, je suis passionné par le dessin et par l’art en général.
+					<br />
+					<br />
+					Pendant mes années de lycée, j’ai appris les fondamentaux de grands logiciels graphiques comme Photoshop, After effects mais aussi 3D comme Cinema 4D et 3DS MAX. Au cours de mon adolescence j’ai réalisé de nombreux projets vidéo sur le thème du jeu vidéo. En effet, je filmais certaines séquences de jeux vidéo que j’affectionne particulièrement afin d’en monter un film à l’aide d’éditeurs de vidéo mais aussi d’animation 3D.
+					<br />
+					<br />
+					Ainsi dans ce projet, je me caractérise plutôt comme le graphiste du groupe ayant pour but de créer un univers émerveillant afin que l’esprit du joueur soit complètement captivé par ce monde. Ce projet est donc une chance pour moi de rendre mes idées plus vivantes que jamais.
+					<br />
+					<br />
+					Ce projet me permettra de développer mes compétences informatiques mais également d’apprendre à travailler sur un projet commun en équipe. Deux atouts indispensables pour un ingénieur. De plus, cette réalisation est pour moi très importante car c’est ma première expérience coopérative sur le thème de l’informatique. Je souhaite donc y acquérir une bonne maturité afin de me mettre en confiance pour les nombreux futurs projets au cours de ma vie étudiante et professionnelle.
+					<br />
+					<br />
+					<h1>Jules "Youl" LAPISARDI</h1>
+					Cette année à Epita est déjà ma deuxième année en supérieur, j’ai donc déjà eu une expérience de travail en groupe pour un projet. Mais cela s’était très mal passé car je me suis retrouvé le seul à travailler sur le projet. Mais cette fois cela n’est pas pareil, car nous sommes motivés et que je m’entends bien avec le groupe.
+					<br />
+					<br />
+					Compte tenu que j’ai déjà eu une expérience de projet, je me suis porté volontaire pour être chef de projet. J’espère donc faire profiter de mon expérience et surtout en acquérir une vraie, avec cette fois un vrai travail de groupe et une vraie synergie.
+					<br />
+					<br />
+					J’espère que ce projet va me permettre de m’améliorer en code, d’apprendre de nouvelles notions que je pourrai utiliser lors de mes études supérieures. Je souhaite aussi acquérir une expérience de groupe qui me sera nécessaire dans le monde du travail.
+					<br />
+					<br />
+					J’attends aussi beaucoup de ce projet car il va me permettre d’apprendre comment fonctionne beaucoup de mécanique du jeu vidéo, la création d’un niveau, les idées de gameplay qu’il faut avoir pour rendre un jeu intéressant.
+					<br />
+					<br />
+					<h1>Axen "Axkod" GEORGET</h1>
+					J’ai fait une Terminale scientifique spécialité mathématiques mais ce n’est pas le système scolaire qui m’a fait découvrir l’informatique. C’est avec une forte attirance vers ce milieu que j’ai découvert les multiples facettes qui le composent.
+					<br />
+					<br />
+					J’ai commencé très jeune à démonter des ordinateurs avec mon père et c’est certainement ce qui a fait naître ma passion. Avec un ordinateur, j’ai pu découvrir le monde fascinant et connecté d’internet et de là j’ai pu m’intéresser à la programmation.
+					<br />
+					<br />
+					Le point de départ a été un tutoriel sur le développement d’applications sur iPhone. Ce tutoriel demandait des connaissances en Objective-C, j’ai donc naturellement fait des recherches sur ce langage. De là je suis tombé sur un tutoriel sur le Site du Zéro (OpenClassroom) sur ce dit langage. Cependant, ce tutoriel demandait des connaissances en C, je me suis donc tourné vers le tutoriel en C du site.
+					<br />
+					<br />
+					C’est à ce moment que je me suis rendu compte que la programmation offrait un nombre quasi infini de possibilités. Après 2 petits projets personnels en C, j’ai voulu tester d’autres langages de programmation. Dans le cadre d’un autre projet j’ai appris l’HTML et le CSS, puis le PHP et MySQL lors d’une amélioration de ce même projet. Par la suite, j’ai utilisé le Pythonnotamment pour résoudre des problèmes de mathématiques plus rapidement. Et, dernièrement j’ai travaillé sur un projet en relation avec le jeu vidéo Minecraft en Java.
+					<br />
+					<br />
+					Toutefois, je n’avais jamais eu la chance de travailler sur un projet d’une telle envergure en groupe. C’est pour cela que j’avais beaucoup d’espérances vis-à-vis de ce projet. Notamment dans l’apprentissage de ce nouveau langage qu’est le C# mais surtout dans le domaine du jeu vidéo, qui est en plus d’être très intéressant est totalement ludique.
+			</td></tr>	
+		</table>
+		<?php include "includes/footer.php"; ?>
+	</body>
+</html>
